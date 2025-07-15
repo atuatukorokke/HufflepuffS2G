6321da5434f6139d96ce0d6e06ef0813be5efdb3
STUDENT\2412002 <ishizaki0555@icloud.com>
2025-07-15 14:54:16 +0900

    タイトルにアニメーションを加えました

---
+++ Hufflepuff/Assets/Scripts/TitleScripts/OperationDescription.cs
@@ -0,0 +1,24 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class OperationDescription : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Animator animator;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        panel.SetActive(false);
+    }
+    public void ShowDescription()
+    {
+        panel.SetActive(true);
+    }
+    public void GaemAnim()
+    {
+        animator.SetBool("OptionOpen", true);
+    }
+}
