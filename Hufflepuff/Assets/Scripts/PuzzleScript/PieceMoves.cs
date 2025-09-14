41c690148ace1047d14dbd4bac4db6a0fb70c690
atuatukorokke <kyoumogohannoisii@gmail.com>
2025-09-15 04:10:49 +0900

    バグがあったら殺してください

--- Hufflepuff/Assets/Scripts/PuzzleScript/PieceMoves.cs
+++ Hufflepuff/Assets/Scripts/PuzzleScript/PieceMoves.cs
@@ -13,17 +13,38 @@ public class PieceMoves : MonoBehaviour
     private PuzzleGrid Pgrid;   // パズルピースを配置するスクリプトを呼び出す
     private PieceCreate Pcreate;    // パズルピースを生成するスクリプト
 
+    private bool isColliding = false;
+
     private void Start()
     {
         Pgrid = FindAnyObjectByType<PuzzleGrid>();
         Pcreate = FindAnyObjectByType<PieceCreate>();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        isColliding = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isColliding = false;
+    }
+
     /// <summary>
-    /// PuzzleGridを呼び出しパズルピースが配置可能かを確認します
+    /// 旧　PuzzleGridを呼び出しパズルピースが配置可能かを確認します
+    /// 新　コライダーの判定確認で配置出来ているかの確認を行います
     /// </summary>
     public void PiecePossible()
     {
-
+        Debug.Log(isColliding);
+        if (isColliding)
+        {
+            Debug.Log("おけてない");
+        }
+        else
+        {
+            Debug.Log("おけてる");
+        }
     }
 }
