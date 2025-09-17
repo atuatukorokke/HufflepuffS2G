68809a57712167b2aeca89b62412777b292c4df1
レイ\ishiz <ishizaki0555@icloud.com>
2025-09-18 07:03:59 +0900

    中ボスの挙動を修正しました

--- Hufflepuff/Assets/Scripts/BulletScripts/Enemy/EnemyMove/BossHealth.cs
+++ Hufflepuff/Assets/Scripts/BulletScripts/Enemy/EnemyMove/BossHealth.cs
@@ -12,13 +12,26 @@ public class BossHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("P_Bullet") || collision.CompareTag("P_Bom"))
+        if (collision.CompareTag("P_Bullet"))
         {
             Destroy(collision.gameObject); // プレイヤーの弾を消す
             if (HP <= 0)
             {
                 GameObject[] bullets = GameObject.FindGameObjectsWithTag("E_Bullet");
                 foreach(GameObject objects in bullets)
+                {
+                    Destroy(objects);
+                }
+                OnDeath?.Invoke(); // 中ボス撃破通知イベントを発火
+                Destroy(this.gameObject); // エネミーの消滅
+            }
+        }
+        else if(collision.CompareTag("P_Bom"))
+        {
+            if (HP <= 0)
+            {
+                GameObject[] bullets = GameObject.FindGameObjectsWithTag("E_Bullet");
+                foreach (GameObject objects in bullets)
                 {
                     Destroy(objects);
                 }
